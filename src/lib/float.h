ed8e6827fecd5f4e96779afcb0d234bbce40e157
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2024-06-26 01:05:56 +0000

    float aparetemente funcionando e booleano mlfqs = true

--- src/lib/float.h
+++ src/lib/float.h
@@ -1,8 +1,7 @@
 #define FLOAT_SHIFT_AMOUNT 17
-#define F mypow(14)
-#define Q 14
+#define F 16384     //2^14
+#define Q 14        
 
-int mypow(int p);
 typedef int float_type;
 
 #define FLOAT_CONST(A) ((float_type)(A * F))
@@ -15,4 +14,4 @@ typedef int float_type;
 #define FLOAT_MULT(A, B) (((int64_t)A) * B / F)
 #define FLOAT_DIV(A, B) (((int64_t)A) * F / B)
 #define FLOAT_INT_PART(A) (A >> FLOAT_SHIFT_AMOUNT)
-#define FLOAT_ROUND(A) (A >= 0 ? ((A + (1 * mypow(Q - 1))) / F):( (A - (1 * mypow(Q - 1))) / F))
+#define FLOAT_ROUND(A) (A >= 0 ? ((A + (1 * 8192)) / F):( (A - (1 * 8192)) / F))
